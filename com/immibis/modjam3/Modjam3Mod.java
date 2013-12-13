d1370ec47563baa6c95e981990959be40270b627
immibis <immibis@gmail.com>
2013-12-13 20:26:09 +1300

    Chickens drop bones, thus justifying this mod's name.

--- com/immibis/modjam3/Modjam3Mod.java
+++ com/immibis/modjam3/Modjam3Mod.java
@@ -1,10 +1,32 @@
 package com.immibis.modjam3;
 
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid="ChickenBones", name="The Chicken Bones Mod", version="1.0")
 //@NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class Modjam3Mod {
-
+	@EventHandler
+	public void init(FMLInitializationEvent evt) {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@ForgeSubscribe
+	public void onLivingDrops(LivingDropsEvent evt) {
+		if(evt.entity instanceof EntityChicken) {
+			evt.drops.add(new EntityItem(evt.entity.worldObj, evt.entity.posX, evt.entity.posY, evt.entity.posZ, new ItemStack(Item.bone)));
+		}
+	}
 }
