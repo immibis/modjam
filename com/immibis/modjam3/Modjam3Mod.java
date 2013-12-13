8e63809d8249810c2eb937b37003662f0b1ac7f4
immibis <immibis@gmail.com>
2013-12-13 20:19:15 +1300

    First commit for Modjam 3.

---
+++ com/immibis/modjam3/Modjam3Mod.java
@@ -0,0 +1,10 @@
+package com.immibis.modjam3;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="ChickenBones", name="The Chicken Bones Mod", version="1.0")
+//@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+public class Modjam3Mod {
+
+}
