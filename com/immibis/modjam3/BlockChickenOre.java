914544f8f0f435668c0cbb9ad4ebfb744913a21a
immibis <immibis@gmail.com>
2013-12-14 20:09:46 +1300

    Chicken face is shown on only one side of each chicken ore block.

--- com/immibis/modjam3/BlockChickenOre.java
+++ com/immibis/modjam3/BlockChickenOre.java
@@ -1,12 +1,17 @@
 package com.immibis.modjam3;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockChickenOre extends Block {
@@ -19,6 +24,35 @@ public class BlockChickenOre extends Block {
 		setHardness(3.0F);
 		setResistance(5.0F);
 		setStepSound(soundStoneFootstep);
+	}
+	
+	private Icon iSide;
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister) {
+		super.registerIcons(par1IconRegister);
+		iSide = par1IconRegister.registerIcon("immibis_modjam3:chicken_ore_side");
+	}
+	
+	@Override
+	public int onBlockPlaced(World par1World, int par2, int par3, int par4, int par5, float par6, float par7, float par8, int par9) {
+		return par1World.isRemote ? 15 : par1World.rand.nextInt(4) + 2;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int par1, int par2) {
+		if(par1 == 4)
+			return blockIcon;
+		else
+			return iSide;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getBlockTexture(IBlockAccess par1iBlockAccess, int par2, int par3, int par4, int par5) {
+		return par1iBlockAccess.getBlockMetadata(par2, par3, par4) == par5 ? blockIcon : iSide;
 	}
 	
 	@Override
