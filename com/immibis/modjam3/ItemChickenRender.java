d7f70a5a84d96f87a11502a65e41726ff3d39171
immibis <immibis@gmail.com>
2013-12-14 21:34:05 +1300

    Chicken staff turns chicken entities into chicken items. Tries and failed to fix dark items after rendering chicken items.

--- com/immibis/modjam3/ItemChickenRender.java
+++ com/immibis/modjam3/ItemChickenRender.java
@@ -1,11 +1,13 @@
 package com.immibis.modjam3;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelChicken;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderChicken;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.passive.EntityChicken;
@@ -57,18 +59,24 @@ public class ItemChickenRender implements IItemRenderer {
 			GL11.glScalef(1.5f, 1.5f, 1.5f);
 			break;
 		case ENTITY:
-			GL11.glTranslatef(-0.5f, -0.5f, -0.5f);
+			GL11.glTranslatef(-0f, -0.5f, -0f);
+			//GL11.glTranslatef(-0.5f, -0.5f, -0.5f);
 			GL11.glScalef(1.2f, 1.2f, 1.2f);
 			break;
 		}
 		RenderManager.instance.renderEngine = Minecraft.getMinecraft().getTextureManager();
 		rc.setRenderManager(RenderManager.instance);
 		rc.doRender(chicken, 0, 0, 0, 0, 0);
-		GL11.glColor3f(1, 1, 1);
+		
+		//GL11.glColor3f(1, 1, 1);
+		//GL11.glDisable(GL11.GL_LIGHTING);
+		//OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+		//GL11.glDisable(GL11.GL_TEXTURE_2D);
+		//OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
+		Tessellator.instance.setBrightness(0x00F000F0);
+		Tessellator.instance.setColorOpaque(255, 255, 255);
+		GL11.glDisable(GL12.GL_RESCALE_NORMAL);
 		GL11.glDisable(GL11.GL_LIGHTING);
-		OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
 		RenderHelper.enableStandardItemLighting();
 	}
 
--- com/immibis/modjam3/ItemChickenStaff.java
+++ com/immibis/modjam3/ItemChickenStaff.java
@@ -1,7 +1,12 @@
 package com.immibis.modjam3;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemChickenStaff extends Item {
 	public ItemChickenStaff(int id) {
@@ -10,6 +15,18 @@ public class ItemChickenStaff extends Item {
 		setUnlocalizedName("immibis_modjam3.chickenstaff");
 		setTextureName("immibis_modjam3:chickenstaff");
 		setCreativeTab(CreativeTabs.tabTools);
+	}
+	
+	@Override
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+		if(entity instanceof EntityChicken) {
+			if(!entity.worldObj.isRemote) {
+				entity.setDead();
+				entity.worldObj.spawnEntityInWorld(new EntityItem(entity.worldObj, entity.posX, entity.posY, entity.posZ, new ItemStack(Modjam3Mod.itemChicken)));
+			}
+			return true;
+		}
+		return super.onLeftClickEntity(stack, player, entity);
 	}
 
 }
