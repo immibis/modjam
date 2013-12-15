72399651ca68cdcf36904e85ce0c50d7df49f05a
immibis <immibis@gmail.com>
2013-12-16 11:57:20 +1300

    Tada! No more squares rendered in the middle of pipes (like there are in Buildcraft pipes)

--- com/immibis/modjam3/BlockChickenPipeRender.java
+++ com/immibis/modjam3/BlockChickenPipeRender.java
@@ -33,84 +33,84 @@ public class BlockChickenPipeRender implements ISimpleBlockRenderingHandler {
 		Tessellator.instance.setBrightness(world.getLightBrightnessForSkyBlocks(x, y, z, 0));
 		Tessellator.instance.setColorOpaque(255, 255, 255);
 		
-		if(!nx && !px && !ny && !py && !nz && !pz) {
+		if(!(nx && px && ny && py && nz && pz)) {
 			renderer.setRenderBounds(min, min, min, max, max, max);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, !nx, !px, !ny, !py, !nz, !pz);
 		}
 		
 		if(nx) {
 			renderer.setRenderBounds(0, min, min, min, max, max);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, false, false, true, true, true, true);
 		}
 		
 		if(px) {
 			renderer.setRenderBounds(max, min, min, 1, max, max);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, false, false, true, true, true, true);
 		}
 		
 		if(ny) {
 			renderer.setRenderBounds(min, 0, min, max, min, max);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, true, true, false, false, true, true);
 		}
 		
 		if(py) {
 			renderer.setRenderBounds(min, max, min, max, 1, max);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, true, true, false, false, true, true);
 		}
 		
 		if(nz) {
 			renderer.setRenderBounds(min, min, 0, max, max, min);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, true, true, true, true, false, false);
 		}
 		
 		if(pz) {
 			renderer.setRenderBounds(min, min, max, max, max, 1);
-			renderStandardBlock(renderer, block, x, y, z);
+			renderStandardBlock(renderer, block, x, y, z, true, true, true, true, false, false);
 		}
 		
 		return true;
 	}
 	
-	private void renderStandardBlock(RenderBlocks rb, Block block, int x, int y, int z) {
+	private void renderStandardBlock(RenderBlocks rb, Block block, int x, int y, int z, boolean nx, boolean px, boolean ny, boolean py, boolean nz, boolean pz) {
 		Tessellator t = Tessellator.instance;
 		Icon i = rb.hasOverrideBlockTexture() ? rb.overrideBlockTexture : Modjam3Mod.blockChickenPipe.getIcon(0, 0);
 		
-		if(rb.renderMinZ > 0) {
+		if(nz) {
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMinY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMinY*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMinY*16));
 		}
 		
-		if(rb.renderMaxZ < 1) {
+		if(pz) {
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMinY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMinY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMaxY*16));
 		}
 		
-		if(rb.renderMinY > 0) {
+		if(ny) {
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMinZ*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMinY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMinZ*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMaxZ*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMaxZ*16));
 		}
 		
-		if(rb.renderMaxY < 1) {
+		if(py) {
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMaxZ*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMaxZ*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMaxX*16), i.getInterpolatedV(rb.renderMinZ*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinX*16), i.getInterpolatedV(rb.renderMinZ*16));
 		}
 		
-		if(rb.renderMinX > 0) {
+		if(nx) {
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinZ*16), i.getInterpolatedV(rb.renderMinY*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxZ*16), i.getInterpolatedV(rb.renderMinY*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxZ*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMinX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinZ*16), i.getInterpolatedV(rb.renderMaxY*16));
 		}
 		
-		if(rb.renderMaxX < 1) {
+		if(px) {
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMinZ, i.getInterpolatedU(rb.renderMinZ*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMaxY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxZ*16), i.getInterpolatedV(rb.renderMaxY*16));
 			t.addVertexWithUV(x+rb.renderMaxX, y+rb.renderMinY, z+rb.renderMaxZ, i.getInterpolatedU(rb.renderMaxZ*16), i.getInterpolatedV(rb.renderMinY*16));
