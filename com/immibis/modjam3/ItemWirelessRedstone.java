31e1b19fcc391af4eaa0b291aebccbd971e4c032
immibis <immibis@gmail.com>
2013-12-15 20:25:55 +1300

    Using wireless redstone prints a message.

--- com/immibis/modjam3/ItemWirelessRedstone.java
+++ com/immibis/modjam3/ItemWirelessRedstone.java
@@ -1,7 +1,11 @@
 package com.immibis.modjam3;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.world.World;
 
 public class ItemWirelessRedstone extends Item {
 	public ItemWirelessRedstone(int id) {
@@ -10,5 +14,12 @@ public class ItemWirelessRedstone extends Item {
 		setUnlocalizedName("immibis_modjam3.wirelessredstone");
 		setTextureName("redstone_dust");
 		setCreativeTab(CreativeTabs.tabRedstone);
+	}
+	
+	@Override
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+		if(par3World.isRemote)
+			par2EntityPlayer.sendChatToPlayer(ChatMessageComponent.createFromTranslationKey("immibis_modjam3.use_wrcbe"));
+		return true;
 	}
 }
