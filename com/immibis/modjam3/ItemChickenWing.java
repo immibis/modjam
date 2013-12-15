e7f3f96801d242f503a57951c40b094709dcae7b
immibis <immibis@gmail.com>
2013-12-15 18:11:56 +1300

    Chicken wings - doubled durability. Left click slowly to go backwards. Left clicking quickly will do nothing.

--- com/immibis/modjam3/ItemChickenWing.java
+++ com/immibis/modjam3/ItemChickenWing.java
@@ -1,6 +1,7 @@
 package com.immibis.modjam3;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -15,29 +16,40 @@ public class ItemChickenWing extends Item {
 		setTextureName("immibis_modjam3:chickenwing");
 		setUnlocalizedName("immibis_modjam3.chickenwing");
 		setMaxStackSize(1);
-		setMaxDamage(1500);
+		setMaxDamage(3000);
 	}
 	
 	@Override
-	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+	public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack) {
+		if(!entityLiving.isSwingInProgress && entityLiving instanceof EntityPlayer)
+			zoom((EntityPlayer)entityLiving, stack, -1);
+		return false;
+	}
+	
+	private void zoom(EntityPlayer player, ItemStack par1ItemStack, int mul) {
+		double speed = 1.5 * mul;
 		
-		double speed = 1.5;
-		
-		Vec3 vec = par3EntityPlayer.getLookVec();
+		Vec3 vec = player.getLookVec();
 		vec.xCoord *= speed;
 		vec.yCoord *= speed;
 		vec.zCoord *= speed;
 		
-		par3EntityPlayer.motionX = clampSpeed(par3EntityPlayer.motionX, vec.xCoord);
-		par3EntityPlayer.motionY = clampSpeed(par3EntityPlayer.motionY, vec.yCoord);
-		par3EntityPlayer.motionZ = clampSpeed(par3EntityPlayer.motionZ, vec.zCoord);
+		player.motionX = adjustSpeed(player.motionX, vec.xCoord);
+		player.motionY = adjustSpeed(player.motionY, vec.yCoord);
+		player.motionZ = adjustSpeed(player.motionZ, vec.zCoord);
+		if(player.motionY >= 0)
+			player.fallDistance = 0;
 		
-		par1ItemStack.damageItem(1, par3EntityPlayer);
-		
+		par1ItemStack.damageItem(1, player);
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+		zoom(par3EntityPlayer, par1ItemStack, 1);
 		return par1ItemStack;
 	}
 
-	private double clampSpeed(double prev, double add) {
+	private double adjustSpeed(double prev, double add) {
 		return prev * 0.8 + add * 0.5;
 	}
 }
